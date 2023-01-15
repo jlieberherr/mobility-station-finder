2fdf89b3bb72ab2b8ec41427cfa901660d007ab2
Johannes Lieberherr <johannes.lieberherr@ttools.ch>
2023-01-15 16:27:38 +0100

    refactoring, __init__.py

---
+++ msf_flask_app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")  # this sets the route to this page
+def home():
+	return jsonify({'Message': "Testing"})
+
+if __name__ == "__main__":
+    app.run()
