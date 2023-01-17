f435f1de796f2f51356ff404424d6700aca6903b
Johannes Lieberherr <johannes.lieberherr@ttools.ch>
2023-01-17 20:32:14 +0100

    first time webserver is working

--- msf_flask_app/__init__.py
+++ msf_flask_app/__init__.py
@@ -1,5 +1,8 @@
+
 from flask import Flask, render_template
 import connexion
+
+from model import load_data, DataContainer
 
 app = connexion.App(__name__, specification_dir="./")
 app.add_api("swagger.yml")
@@ -9,4 +12,5 @@ def home():
 	return render_template("map.html")
 
 if __name__ == "__main__":
+    load_data()
     app.run()
