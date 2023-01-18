83213b420b160a8df966f6e72b18f9eb0df6bbd9
Johannes Lieberherr <johannes.lieberherr@ttools.ch>
2023-01-18 20:40:04 +0100

    refactoring

---
+++ app_server.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import connexion
+
+from msf_flask_app.model import load_data
+
+app = connexion.App(__name__)
+app.add_api("swagger.yml")
+
+@app.route("/")
+def home():
+	return render_template("map.html")
+
+if __name__ == "__main__":
+    load_data()
+    app.run()
