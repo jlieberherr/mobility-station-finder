a2e5e8aa5be6307dae353ee1bd3de5b87f32dcb8
Johannes Lieberherr <johannes.lieberherr@ttools.ch>
2023-04-02 13:28:07 +0200

    with right click popup (not setting a merker yet)

--- static/js/mainpage.js
+++ static/js/mainpage.js
@@ -31,13 +31,53 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-const info = document.querySelector(".info");
+
+
+
+// Add a contextmenu event listener to the map
+map.on('contextmenu', function (e) {
+    // Create the popup content
+    const form = `
+    <form>
+      <label id="origField">Hier Startpunkt setzen</label>
+      <br>
+      <label id="destField">Hier Zielpunkt setzen</label>
+    </form>
+  `;
+
+    // Open the popup when the map is right-clicked
+    const popup = L.popup().setLatLng(e.latlng).setContent(form).openOn(map);
+    const formElement = popup.getElement().querySelector('form');
+
+    const origField = formElement.querySelector("#origField");
+    const destField = formElement.querySelector("#destField");
+
+    origField.addEventListener('click', function (event) {
+        console.log(`origField clicked: ${e.latlng}`);
+        map.closePopup(popup);
+    });
+
+    destField.addEventListener('click', function (event) {
+        console.log(`destField clicked: ${e.latlng}`);
+        map.closePopup(popup);
+    });
+});
+
+
+
+
+
+
 const orig = document.querySelector("#orig");
 const dest = document.querySelector("#dest");
+const origCoords = document.querySelector(".orig-coords");
+origCoords.value = "Startkoordinaten: "
+const destCoords = document.querySelector(".dest-coords");
+destCoords.value = "Zielkoordinaten: "
 const searchButton = document.querySelector(".run-search");
 const slider = document.querySelector(".slider");
 
-let bestMobilityStationsPerVTTS = {};
+let bestMobilityStationsPerVTTS = null;
 let origMarker = null;
 let destMarker = null;
 let stationMarkers = [];
@@ -78,16 +118,18 @@ function nominatim(currentValue) {
     });
 }
 
-function clearStationsMarkers() {
+function clearStationData() {
     stationMarkers.forEach((marker) => {
         marker.remove();
     });
     stationMarkers = [];
+    bestMobilityStationsPerVTTS = null;
+    checkForSlider();
 }
 
 function addMarkerToMap(point, object) {
 
-    clearStationsMarkers();
+    clearStationData();
 
     const display_name = object.properties.display_name;
     const coords = object.geometry.coordinates.reverse();
@@ -98,6 +140,7 @@ function addMarkerToMap(point, object) {
         }
         title = `Startpunkt: ${display_name}`
         icon = origIcon;
+        updateTextField(origCoords, `${origCoords.value}${coords[0]}, ${coords[1]}`)
     }
     else if (point == "dest") {
         if (destMarker != null) {
@@ -105,6 +148,7 @@ function addMarkerToMap(point, object) {
         }
         title = `Endpunkt: ${display_name}`;
         icon = destIcon;
+        updateTextField(destCoords, `${destCoords.value}${coords[0]}, ${coords[1]}`)
     }
 
     const marker = L.marker(coords, {
@@ -113,6 +157,27 @@ function addMarkerToMap(point, object) {
         icon: icon,
     });
 
+    marker.addTo(map).bindPopup(title);
+
+    marker.on("dragend", function (e) {
+        const coords = e.target.getLatLng();
+        console.log(`dragend: ${coords}`)
+        if (point == "orig") {
+            field = origCoords;
+            orig.value = "";
+        }
+        else if (point == "dest") {
+            field = destCoords;
+            dest.value = "";
+        }
+        updateTextField(field, `${field.value}${coords.lat}, ${coords.lng}`)
+        checkForSearch();
+        clearStationData();
+        checkForSlider();
+
+    });
+
+
     if (point == "orig") {
         origMarker = marker;
     }
@@ -120,15 +185,14 @@ function addMarkerToMap(point, object) {
         destMarker = marker;
     }
 
-    // add marker to map
-    marker.addTo(map).bindPopup(title);
+    checkForSearch();
 
 }
 
 
 function getBestMobilityStations() {
 
-    bestMobilityStationsPerVTTS = {}
+    clearStationData();
 
     // get result form an rest interface
     const api = `http://127.0.0.1:5000/api/get-best-mobility-stations?orig_easting=${origMarker._latlng.lng}&orig_northing=${origMarker._latlng.lat}&dest_easting=${destMarker._latlng.lng}&dest_northing=${destMarker._latlng.lat}`;
@@ -140,23 +204,20 @@ function getBestMobilityStations() {
         .then((data) => {
             console.log(data)
             bestMobilityStationsPerVTTS = JSON.parse(data);
-            const vTTS = slider.value;
-            showBestMobilityStations(vTTS);
+            if (checkForSlider()) {
+                const vTTS = slider.value;
+                showBestMobilityStations(vTTS);
+            }
         })
 }
 
 function showBestMobilityStations(vTTS) {
-    stationMarkers.forEach((marker) => {
-        marker.remove();
-    });
-    stationMarkers = [];
-
     bestMobilityStations = bestMobilityStationsPerVTTS[vTTS];
     bestMobilityStations.forEach((station) => {
         stationName = station["Name"]
         easting = station["easting"]
         northing = station["northing"]
-        marker = L.circleMarker([northing, easting], { fillColor: "red" , color: "red"})
+        marker = L.circleMarker([northing, easting], { fillColor: "red", color: "red" })
         stationMarkers.push(marker)
         marker.addTo(map).bindPopup(stationName);
     }
@@ -176,6 +237,33 @@ searchButton.addEventListener("click", () => {
 });
 
 
+function updateTextField(element, newText) {
+    element.textContent = newText;
+}
+
+function checkForSearch() {
+    if (origMarker != null && destMarker != null) {
+        searchButton.disabled = false;
+        return true;
+    }
+    else {
+        searchButton.disabled = true;
+        return false;
+    }
+}
+
+function checkForSlider() {
+    if (bestMobilityStationsPerVTTS != null) {
+        slider.disabled = false;
+        return true;
+    }
+    else {
+        slider.disabled = true;
+        return false;
+    }
+}
+
+
 window.addEventListener("DOMContentLoaded", function () {
 
     ["orig", "dest"].forEach((point) => {
@@ -192,10 +280,17 @@ window.addEventListener("DOMContentLoaded", function () {
             onReset: () => {
                 if (point == "orig") {
                     origMarker.remove();
+                    updateTextField(origCoords, origCoords.value)
+                    origMarker = null;
                 }
                 else if (point == "dest") {
                     destMarker.remove();
+                    updateTextField(destCoords, destCoords.value)
+                    destMarker = null;
                 }
+                checkForSearch();
+                clearStationData();
+                checkForSlider();
             },
             // the method presents no results
             noResults: ({ currentValue, template }) =>
