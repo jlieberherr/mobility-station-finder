1fdd230f064c26a292c627d494f06e16fcd8b104
Johannes Lieberherr <johannes.lieberherr@ttools.ch>
2023-04-10 18:46:18 +0200

    with ojp and osrm webrequests, polylines on map and journey information retrival

--- static/js/mainpage.js
+++ static/js/mainpage.js
@@ -47,6 +47,9 @@ let bestMobilityStationsPerVTTS = null;
 let origMarker = null;
 let destMarker = null;
 let stationMarkers = [];
+let polylineFeatureGroup = null;
+let journeyInfo = null;
+let xmlDocForTesting = null;
 
 
 // Add a contextmenu event listener to the map
@@ -184,6 +187,10 @@ function nominatim(currentValue) {
 
 
 function clearStationMarkers() {
+    if (polylineFeatureGroup != null) {
+        polylineFeatureGroup.remove();
+    };
+    polylineFeatureGroup == null;
     stationMarkers.forEach((marker) => {
         marker.remove();
     });
@@ -212,7 +219,6 @@ function getBestMobilityStations() {
     // fetch data
     fetch(api)
         .then((response) => {
-            console.log(response.status);
             if (response.status == 500) {
                 alert("Es ist ein unbekannter Fehler aufgetreten. Bitte versuchen Sie es mit anderen Start- und Zielkoordinaten oder versuchen Sie es später erneut.");
                 throw new Error(response);
@@ -239,6 +245,24 @@ function getBestMobilityStations() {
         });
 }
 
+const apiKey = "eyJvcmciOiI2NDA2NTFhNTIyZmEwNTAwMDEyOWJiZTEiLCJpZCI6IjdjZjQ3OWViZjA5ZTRhZDQ5OTFiYzgyMzM0NjA5NzkwIiwiaCI6Im11cm11cjEyOCJ9"
+
+const headers = {
+    'Authorization': `Bearer ${apiKey}`,
+    'Content-Type': 'application/xml'
+};
+
+
+function getTypeOfTripLeg(tripLeg){
+    // check if "ojp:ContinuousLeg" is in the tripLeg
+    if (tripLeg.getElementsByTagName("ojp:ContinuousLeg").length > 0) {
+        return "ContinuousLeg";
+    } else if (tripLeg.getElementsByTagName("ojp:TimedLeg").length > 0) {
+        return "TimedLeg";
+    } else if (tripLeg.getElementsByTagName("ojp:TransferLeg").length > 0) {
+        return "TransferLeg";
+    } else {console.log("Error: Unknown type of trip leg: " + tripLeg)};
+}
 
 function showBestMobilityStations(vTTS) {
     clearStationMarkers();
@@ -250,6 +274,113 @@ function showBestMobilityStations(vTTS) {
         marker = L.circleMarker([northing, easting], { fillColor: "red", color: "red" })
         stationMarkers.push(marker)
         marker.addTo(map).bindPopup(stationName);
+        marker.addEventListener("click", (e) => {
+            // init polylineFeatureGroup
+            if (polylineFeatureGroup != null) {
+                // remove polylineFeatureGroup from map
+                polylineFeatureGroup.remove();
+                // create new polylineFeatureGroup
+            }
+            polylineFeatureGroup = L.featureGroup();
+            journeyInfo = {};
+            ptInfos = [];
+            var this_marker = e.target;
+            xmlDoc = getOJPTripRequestXMLObjetct(this_marker);
+            const url_ojp = 'https://api.opentransportdata.swiss/ojp2020';
+            xml_str = serializer.serializeToString(xmlDoc);
+            axios.post(url_ojp, xml_str, { headers })
+            .then(response => {
+                const parser = new DOMParser();
+                const xmlDoc = parser.parseFromString(response.data, "text/xml");
+                xmlDocForTesting = xmlDoc;
+                // iterate over all trips
+                const trip = xmlDoc.getElementsByTagName("ojp:TripResult")[0];
+                const tripLegs = trip.getElementsByTagName("ojp:TripLeg");
+                for (let i = 0; i < tripLegs.length; i++) {
+                    tripLeg = tripLegs[i];
+                    const legType = getTypeOfTripLeg(tripLeg);
+                    var color = null;
+                    if (legType == "ContinuousLeg" || legType == "TimedLeg") {
+                        if (legType == "ContinuousLeg") {
+                            color = "green";
+                            const continousLeg = tripLeg.getElementsByTagName("ojp:ContinuousLeg")[0];
+                            const legMode = continousLeg.getElementsByTagName("ojp:Service")[0].getElementsByTagName("ojp:IndividualMode")[0].textContent;
+                            const legStart = continousLeg.getElementsByTagName("ojp:LegStart")[0];
+                            const startName = legStart.getElementsByTagName("ojp:LocationName")[0].getElementsByTagName("ojp:Text")[0].textContent;
+                            const legEnd = continousLeg.getElementsByTagName("ojp:LegEnd")[0];
+                            const endName = legEnd.getElementsByTagName("ojp:LocationName")[0].getElementsByTagName("ojp:Text")[0].textContent;
+                            const startTime = new Date(continousLeg.getElementsByTagName("ojp:TimeWindowStart")[0].textContent);
+                            const endTime = new Date(continousLeg.getElementsByTagName("ojp:TimeWindowEnd")[0].textContent);
+                            const legInfo = {"legMode": legMode, "startName": startName, "endName": endName, "startTime": startTime, "endTime": endTime};
+                            ptInfos.push(legInfo);
+                        } else {
+                            color = "blue"
+                            const timedLeg = tripLeg.getElementsByTagName("ojp:TimedLeg")[0];
+                            const legMode = timedLeg.getElementsByTagName("ojp:Service")[0].getElementsByTagName("ojp:Mode")[0].getElementsByTagName("ojp:Name")[0].getElementsByTagName("ojp:Text")[0].textContent;
+                            const legBoard = timedLeg.getElementsByTagName("ojp:LegBoard")[0];
+                            const startName = legBoard.getElementsByTagName("ojp:StopPointName")[0].getElementsByTagName("ojp:Text")[0].textContent;
+                            const startTime = new Date(legBoard.getElementsByTagName("ojp:ServiceDeparture")[0].getElementsByTagName("ojp:TimetabledTime")[0].textContent);
+                            const legAlight = timedLeg.getElementsByTagName("ojp:LegAlight")[0];
+                            const endName = legAlight.getElementsByTagName("ojp:StopPointName")[0].getElementsByTagName("ojp:Text")[0].textContent;
+                            const endTime = new Date(legAlight.getElementsByTagName("ojp:ServiceArrival")[0].getElementsByTagName("ojp:TimetabledTime")[0].textContent);
+                            const legInfo = {"legMode": legMode, "startName": startName, "endName": endName, "startTime": startTime, "endTime": endTime};
+                            ptInfos.push(legInfo);
+                        };
+                        // add polyline with all positions to map
+                        const positions = tripLeg.getElementsByTagName("ojp:Position");
+                        var pointList = [];
+                        for (let j = 0; j < positions.length; j++) {
+                            var position = positions[j];
+                            var easting = position.getElementsByTagName("siri:Longitude")[0].textContent
+                            var northing = position.getElementsByTagName("siri:Latitude")[0].textContent
+                            var point = new L.LatLng(northing, easting);
+                            // add point to pointList
+                            pointList.push(point);
+                        }
+                        var polyline = new L.polyline(pointList, {
+                            color: color,
+                        });
+                        polylineFeatureGroup.addLayer(polyline);
+                    } else if (legType == "TransferLeg") {
+                        // TODO
+                    } else {
+                        console.log("Error: Unknown type of trip leg: " + tripLeg);
+                    }
+                    journeyInfo["ptInfos"] = ptInfos;
+                }
+            })
+            .catch(error => {
+                console.error(error);
+            });
+            var stationEasting = this_marker.getLatLng().lng;
+            var stationNorthing = this_marker.getLatLng().lat;
+            var destEasting = destMarker.getLatLng().lng;
+            var destNorthing = destMarker.getLatLng().lat;
+            const url_orm = `http://router.project-osrm.org/route/v1/driving/${stationEasting},${stationNorthing};${destEasting},${destNorthing}?geometries=geojson`;
+            fetch(url_orm)
+            .then((response) => {
+                return response.json();})
+            .then((data) => {
+                var pointList = [];
+                const coords = data["routes"][0]["geometry"]["coordinates"];
+                const duration = parseFloat(data["routes"][0]["duration"]) / 60.0;
+                const distance = parseFloat(data["routes"][0]["distance"]) / 1000.0;
+                journeyInfo["mobilityInfos"] = {"startName": this_marker._popup._content, "endName": destMarker._popup._content, "duration": duration, "distance": distance};
+                for (let i = 0; i < coords.length; i++) {
+                    var point = new L.LatLng(coords[i][1], coords[i][0]);
+                    // add point to pointList
+                    pointList.push(point);
+                };
+                var polyline = new L.polyline(pointList, {
+                    color: "red",
+                });
+                polylineFeatureGroup.addLayer(polyline);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+            polylineFeatureGroup.addTo(map);
+        });
     }
     )
 }
@@ -330,3 +461,74 @@ window.addEventListener("DOMContentLoaded", function () {
 
     });
 });
+
+const ojp_trip_request_template = `<?xml version="1.0" encoding="utf-8"?>
+<OJP xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns="http://www.siri.org.uk/siri" version="1.0" xmlns:ojp="http://www.vdv.de/ojp" xsi:schemaLocation="http://www.siri.org.uk/siri ../ojp-xsd-v1.0/OJP.xsd">
+    <OJPRequest>
+        <ServiceRequest>
+            <RequestTimestamp>2023-04-10T09:10:32.267Z</RequestTimestamp>
+            <RequestorRef>API-Explorer</RequestorRef>
+            <ojp:OJPTripRequest>
+                <RequestTimestamp>2023-04-10T09:10:32.267Z</RequestTimestamp>
+                <ojp:Origin>
+                    <ojp:PlaceRef>
+                        <ojp:GeoPosition>
+                            <Longitude>7.446683</Longitude>
+                            <Latitude>46.928306</Latitude>
+                        </ojp:GeoPosition>
+                        <ojp:LocationName>
+                            <ojp:Text>Start</ojp:Text>
+                        </ojp:LocationName>
+                    </ojp:PlaceRef>
+                    <ojp:DepArrTime>2023-04-12T10:00:00</ojp:DepArrTime>
+                </ojp:Origin>
+                <ojp:Destination>
+                    <ojp:PlaceRef>
+                        <ojp:GeoPosition>
+                            <Longitude>8.55408</Longitude>
+                            <Latitude>47.36488</Latitude>
+                        </ojp:GeoPosition>
+                        <ojp:LocationName>
+                            <ojp:Text>Ziel</ojp:Text>
+                        </ojp:LocationName>
+                    </ojp:PlaceRef>
+                </ojp:Destination>
+                <ojp:Params>
+                    <ojp:IncludeTrackSections>true</ojp:IncludeTrackSections>
+                    <ojp:IncludeLegProjection>true</ojp:IncludeLegProjection>
+                    <ojp:IncludeTurnDescription></ojp:IncludeTurnDescription>
+                    <ojp:IncludeIntermediateStops></ojp:IncludeIntermediateStops>
+                </ojp:Params>
+            </ojp:OJPTripRequest>
+        </ServiceRequest>
+    </OJPRequest>
+</OJP>`;
+
+const serializer = new XMLSerializer();
+
+function setOriginCoordinates(xmlDoc) {
+    const originNode = xmlDoc.getElementsByTagName("ojp:Origin")[0];
+    const longitude = originNode.getElementsByTagName("Longitude")[0];
+    const latitude = originNode.getElementsByTagName("Latitude")[0];
+    longitude.textContent = origMarker.getLatLng().lng;
+    latitude.textContent = origMarker.getLatLng().lat;
+}
+
+function setStationCoordinates(xmlDoc, marker) {
+    const destinationNode = xmlDoc.getElementsByTagName("ojp:Destination")[0];
+    const longitude = destinationNode.getElementsByTagName("Longitude")[0];
+    const latitude = destinationNode.getElementsByTagName("Latitude")[0];
+    const locationName = destinationNode.getElementsByTagName("ojp:LocationName")[0].getElementsByTagName("ojp:Text")[0];
+    longitude.textContent = marker.getLatLng().lng;
+    latitude.textContent = marker.getLatLng().lat;
+    locationName.textContent = marker._popup._content;
+}
+
+function getOJPTripRequestXMLObjetct(marker) {
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(ojp_trip_request_template, "text/xml");
+    setOriginCoordinates(xmlDoc);
+    setStationCoordinates(xmlDoc, marker);
+    return xmlDoc;
+}
+
